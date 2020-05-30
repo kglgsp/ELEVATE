f620dbe97a8b26eae3bc367e339a13c09819684f
Katherine Legaspi <kathlgsp@gmail.com>
2020-05-30 16:45:44 -0700

    added components

---
+++ elevate/src/components/HomePage/HomePage.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './HomePage.css';
+
+class HomePage extends Component {
+  // constructor(props){
+    // super(props);
+    // this.state = {};
+  // }
+
+  // componentWillMount(){}
+  // componentDidMount(){}
+  // componentWillUnmount(){}
+
+  // componentWillReceiveProps(){}
+  // shouldComponentUpdate(){}
+  // componentWillUpdate(){}
+  // componentDidUpdate(){}
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default HomePage;
